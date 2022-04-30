7739943e618af295be47c7ebd8b1bed18cfa19dc
muhyasin89 <muhyasin89@gmail.com>
2022-04-30 11:17:51 +0700

    [main]add new image

---
+++ src/page/home.js
@@ -0,0 +1,18 @@
+import logoBookMark from "../imgs/logo-bookmark.svg"
+
+function Home(){
+    return (
+        <header className="font-Poppins">
+            <nav className="container flex item-center py-4 mt-4 sm:mt-12">
+                <div className="py-1">
+                    <img src={logoBookMark} />
+                    <ul className="hidden sm:flex flex-1 justify-end items-center gap-12 text-bookmark-blue">
+                        Test
+                    </ul>
+                </div>
+            </nav>
+        </header>
+    );
+}
+
+export default Home;
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "bookmark-purple": "#5267DF",
+        "bookmark-red": "#FA5959",
+        "bookmark-blue": "#242A45",
+        "bookmark-grey": "#9194A2",
+        "bookmark-white": "#f7f7f7"
+      }
+    },
+    fontFamily: {
+      Poppins: ["Popins, sans-serif"],
+    },
+    container:{
+      center: true,
+      padding: "1rem",
+    }
+  },
+  plugins: [],
+}
